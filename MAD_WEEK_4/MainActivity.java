f3f5bbff482c1e6626a509090cecd7e2f956d10f
AVidhanR <itsvidhanreddy@gmail.com>
2024-04-23 11:16:18 +0530

    Create MainActivity.java

---
+++ MAD_WEEK_4/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.linearandrelativelayout;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Call the superclass onCreate method
+        super.onCreate(savedInstanceState);
+
+        // Set the content view to the activity_main layout
+        setContentView(R.layout.activity_main);
+    }
+}
